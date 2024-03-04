7f164f8f934ff20b3740149d621863a6bd8c3af0
shravan826 <suthar826@gmail.com>
2024-03-04 14:45:28 +0530

    Created the configuration for file handling related stuff

---
+++ src/utils/FileUploadOnCloud.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return "File path doesn't exist"
+    }
+    const response = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
+    //file has been uploaded successfull
+    console.log("File is uploaded successfully and the resp is: " + response);
+    return response.url;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);//It is for removing the locally saved temporary file as the upload got failed.
+  }
+}
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) { console.log(result); });
